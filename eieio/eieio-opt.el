1988a65657ced6b7a37c79fda8c67768ef5db08d
zappo <zappo@users.sourceforge.net>
2010-04-09 02:11:42 +0000

    (eieio-describe-class, eieio-describe-generic): call cedet-called-interactively-p instead of interactive-p

--- eieio/eieio-opt.el
+++ eieio/eieio-opt.el
@@ -1,9 +1,9 @@
 ;;; eieio-opt.el -- eieio optional functions (debug, printing, speedbar)
 
-;;; Copyright (C) 1996, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2008, 2009 Eric M. Ludlam
+;;; Copyright (C) 1996, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2008, 2009, 2010 Eric M. Ludlam
 ;;
 ;; Author: <zappo@gnu.org>
-;; RCS: $Id: eieio-opt.el,v 1.39 2009-07-12 14:24:06 zappo Exp $
+;; RCS: $Id: eieio-opt.el,v 1.40 2010-04-09 02:11:42 zappo Exp $
 ;; Keywords: OO, lisp
 ;;                                                                          
 ;; This program is free software; you can redistribute it and/or modify
@@ -82,7 +82,7 @@ Optional HEADERFCN should be called to insert a few bits of info first."
   (interactive (list (eieio-read-class "Class: ")))
   (with-output-to-temp-buffer (help-buffer) ;"*Help*"
     (help-setup-xref (list #'eieio-describe-class class headerfcn)
-		     (interactive-p))
+		     (cedet-called-interactively-p))
 
     (when headerfcn (funcall headerfcn))
 
@@ -319,7 +319,8 @@ Also extracts information about all methods specific to this generic."
   (if (not (generic-p generic))
       (signal 'wrong-type-argument '(generic-p generic)))
   (with-output-to-temp-buffer (help-buffer) ; "*Help*"
-    (help-setup-xref (list #'eieio-describe-generic generic) (interactive-p))
+    (help-setup-xref (list #'eieio-describe-generic generic)
+		     (cedet-called-interactively-p))
 
     (prin1 generic)
     (princ " is a generic function")
