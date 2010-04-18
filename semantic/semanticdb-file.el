059f3f67423857e865e1dc901fb7207178a48486
zappo <zappo@users.sourceforge.net>
2010-04-18 20:22:08 +0000

    (semanticdb-project-database-file::semanticdb-save-db): Simplify error skip msg.

--- semantic/semanticdb-file.el
+++ semantic/semanticdb-file.el
@@ -1,10 +1,10 @@
 ;;; semanticdb-file.el --- Save a semanticdb to a cache file.
 
-;;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2007, 2008, 2009 Eric M. Ludlam
+;;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2007, 2008, 2009, 2010 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 ;; Keywords: tags
-;; X-RCS: $Id: semanticdb-file.el,v 1.48 2010-03-15 13:40:55 xscript Exp $
+;; X-RCS: $Id: semanticdb-file.el,v 1.49 2010-04-18 20:22:08 zappo Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -246,7 +246,7 @@ If DB is not specified, then use the current database."
 	   ;; @todo - It should ask if we are not called from a hook.
 	   ;;         How?
 	   (if (or supress-questions
-		   (y-or-n-p (format "Skip Error: %S ?" (car (cdr foo)))))
+		   (y-or-n-p (format "Skip Error: %s ?" (car (cdr foo)))))
 	       (message "Save Error: %S: %s" (car (cdr foo))
 			objname)
 	     (error "%S" (car (cdr foo))))))))
