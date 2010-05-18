f269022bafb5cce84a5e416407677797be29c6ba
zappo <zappo@users.sourceforge.net>
2010-05-18 00:42:27 +0000

    (ede-linux-project::initialize-instance): Fix doc string.

--- ede/ede-linux.el
+++ ede/ede-linux.el
@@ -1,9 +1,9 @@
 ;;; ede-linux.el --- Special project for Linux
 
-;; Copyright (C) 2008, 2009 Eric M. Ludlam
+;; Copyright (C) 2008, 2009, 2010 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: ede-linux.el,v 1.6 2010-03-15 13:40:54 xscript Exp $
+;; X-RCS: $Id: ede-linux.el,v 1.7 2010-05-18 00:42:27 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -136,7 +136,7 @@ All directories need at least one target.")
 
 (defmethod initialize-instance ((this ede-linux-project)
 				&rest fields)
-  "Make sure the :file is fully expanded."
+  "Make sure the targets slot is bound."
   (call-next-method)
   (unless (slot-boundp this 'targets)
     (oset this :targets nil)))
