2d98368cb6c51b8714fe2a411857ca77336d5e53
zappo <zappo@users.sourceforge.net>
2010-06-13 01:16:52 +0000

    (cedet-idutils-make-command): New option. (cedet-idutils-mkid-call): New util fcn.

--- common/cedet-idutils.el
+++ common/cedet-idutils.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2009, 2010 Eric M. Ludlam
 ;;
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: cedet-idutils.el,v 1.5 2010-04-09 02:22:34 zappo Exp $
+;; X-RCS: $Id: cedet-idutils.el,v 1.6 2010-06-13 01:16:52 zappo Exp $
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -39,6 +39,12 @@
 ;;;###autoload
 (defcustom cedet-idutils-token-command "lid"
   "Command name for the ID Utils executable for searching for tokens."
+  :type 'string
+  :group 'cedet)
+
+;;;###autoload
+(defcustom cedet-idutils-make-command "mkid"
+  "Command name for the ID Utils executable for creating token databases."
   :type 'string
   :group 'cedet)
 
@@ -100,6 +106,20 @@ Return the created buffer with with program output."
       (setq default-directory cd)
       (erase-buffer))
     (apply 'call-process cedet-idutils-token-command
+	   nil b nil
+	   flags)
+    b))
+
+(defun cedet-idutils-mkid-call (flags)
+  "Call ID Utils lid with the list of FLAGS.
+Return the created buffer with with program output."
+  (let ((b (get-buffer-create "*CEDET mkid*"))
+	(cd default-directory)
+	)
+    (with-current-buffer b
+      (setq default-directory cd)
+      (erase-buffer))
+    (apply 'call-process cedet-idutils-make-command
 	   nil b nil
 	   flags)
     b))
