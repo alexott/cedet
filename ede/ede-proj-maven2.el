68465c107a77a5a33f42e43ec68dea1f877b8ed4
ottalex <ottalex@users.sourceforge.net>
2010-08-01 18:12:04 +0000

    small refactoring - move maven command into separate variable

--- ede/ede-proj-maven2.el
+++ ede/ede-proj-maven2.el
@@ -2,7 +2,7 @@
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
 ;; Joakim Verona <joakim@verona.se>
-;; X-RCS: $Id: ede-proj-maven2.el,v 1.5 2009-03-19 19:37:55 joakimv Exp $
+;; X-RCS: $Id: ede-proj-maven2.el,v 1.6 2010-08-01 18:12:04 ottalex Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -22,7 +22,7 @@
 ;;; Commentary:
 ;;
 
-;; Notes: 
+;; Notes:
 
 ;; - Maven2 is very different from Maven1, so this file wont be
 ;; useful for Maven1 projects.
@@ -59,7 +59,7 @@
 
 ;;TODO
 
-;; 
+;;
 ;; BUG: (this bug-description is somewhat based on a missunderstanding
 ;; of EDE, FIXME) in projects with a root pom and modules with poms
 ;; residing in child directories of the root pom, "compile" sometimes
@@ -114,7 +114,7 @@
   (let ((ans nil))
     (while (and dir (not ans))
       (when (file-exists-p (expand-file-name "pom.xml" dir))
-	(setq ans dir))
+        (setq ans dir))
       (setq dir (ede-up-directory dir)))
     ans))
 
@@ -126,11 +126,11 @@
 DIR is the directory to search from."
   ;;TODO this is cloned from ede-emacs-file-existing, should be refactorable
   (let ((projs ede-maven2-project-list)
-	(ans nil))
+        (ans nil))
     (while (and projs (not ans))
       (let ((root (ede-project-root-directory (car projs))))
-	(when (string-match (concat "^" (regexp-quote root)) dir)
-	  (setq ans (car projs))))
+        (when (string-match (concat "^" (regexp-quote root)) dir)
+          (setq ans (car projs))))
       (setq projs (cdr projs)))
     ans))
 
@@ -148,8 +148,8 @@ ROOTPROJ is nil, since there is only one project."
                                  :name "maven dir" ; make fancy name from dir here.
                                  :directory dir
                                  :file
-				 (expand-file-name "pom.xml" dir)
-				 )))
+                                 (expand-file-name "pom.xml" dir)
+                                 )))
          ;; (message "adding %s to global proj list" this)
          (ede-add-project-to-global-list this)
          ;;TODO the above seems to be done somewhere else, maybe ede-load-project-file
@@ -180,7 +180,7 @@ All directories need at least one target.")
 
 
 (defmethod initialize-instance ((this ede-maven2-project)
-				&rest fields)
+                                &rest fields)
   "Make sure the :targets is setup."
   (call-next-method)
   (unless (slot-boundp this 'targets)
@@ -193,42 +193,45 @@ All directories need at least one target.")
 ;;  - 2nd 2 compiles the child project the current file is a member of
 ;;maven error messages are recognized by emacs23
 
+(defvar ede-maven2-compile-command "mvn install"
+  "Compile command for Maven2 project")
+
 (defmethod project-compile-project ((obj ede-maven2-project) &optional command)
   "Compile the entire current project OBJ.
 Argument COMMAND is the command to use when compiling."
   ;; we need to be in the proj root dir for this to work
   (let ((default-directory (ede-project-root-directory obj)))
-    (compile "mvn install")))
+    (compile ede-maven2-compile-command)))
 
 
 (defmethod project-compile-target ((obj ede-maven2-target-java) &optional command)
   "Compile the current target OBJ.
 Argument COMMAND is the command to use for compiling the target."
   (let* ((default-directory (ede-maven2-project-root (oref obj :path))))
-    (compile "mvn install")))
+    (compile ede-maven2-compile-command)))
 
 (defmethod project-compile-target ((obj ede-maven2-target-misc) &optional command)
   "Compile the current target OBJ.
 Argument COMMAND is the command to use for compiling the target."
   (let* ((default-directory (ede-maven2-project-root (oref obj :path))))
-    (compile "mvn install")))
+    (compile ede-maven2-compile-command)))
 
 ;;; File Stuff
 ;;
  (defmethod ede-project-root-directory ((this ede-maven2-project)
- 				       &optional file)
+                                       &optional file)
    "Return the root for THIS Maven project with file."
 ;;   (file-name-directory (oref this file))
    (oref this :directory)
    )
-   
+
 
  (defmethod ede-project-root ((this ede-maven2-project))
    "Return my root."
    this)
 
  (defmethod ede-find-subproject-for-directory ((proj ede-maven2-project)
- 					      dir)
+                                              dir)
    "Return PROJ, for handling all subdirs below DIR."
    proj)
 
@@ -243,8 +246,8 @@ Argument COMMAND is the command to use for compiling the target."
   (let ((match nil))
     (dolist (T targets)
       (when (and (object-of-class-p T class)
-		 (string= (oref T :path) dir))
-	(setq match T)
+                 (string= (oref T :path) dir))
+        (setq match T)
       ))
     match))
 
@@ -252,22 +255,22 @@ Argument COMMAND is the command to use for compiling the target."
   "Find an EDE target in PROJ for BUFFER.
 If one doesn't exist, create a new one for this directory."
   (let* ((ext (file-name-extension (buffer-file-name buffer)))
-	 (cls (cond ((not ext)
-		     'ede-maven2-target-misc)
-		    ((string-match "java" ext)
-		     'ede-maven2-target-java)
-		    (t 'ede-maven2-target-misc)))
-	 (targets (oref proj targets))
-	 (dir default-directory)
-	 (ans (ede-maven2-find-matching-target cls dir targets))
-	 )
+         (cls (cond ((not ext)
+                     'ede-maven2-target-misc)
+                    ((string-match "java" ext)
+                     'ede-maven2-target-java)
+                    (t 'ede-maven2-target-misc)))
+         (targets (oref proj targets))
+         (dir default-directory)
+         (ans (ede-maven2-find-matching-target cls dir targets))
+         )
     (when (not ans)
-      (setq ans (make-instance 
-		 cls 
-		 :name (file-name-nondirectory
-			(directory-file-name dir))
-		 :path dir
-		 :source nil))
+      (setq ans (make-instance
+                 cls
+                 :name (file-name-nondirectory
+                        (directory-file-name dir))
+                 :path dir
+                 :source nil))
       (object-add-to-list proj :targets ans)
       )
     ans))
@@ -277,15 +280,15 @@ If one doesn't exist, create a new one for this directory."
 
 ;;;###autoloa d
 (add-to-list 'ede-project-class-files
-	     (ede-project-autoload "maven2"
-	      :name "MAVEN2"
-	      :file 'ede-proj-maven2
-	      :proj-file "pom.xml"
-	      :proj-root 'ede-maven2-project-root
-	      :load-type 'ede-maven2-load
-	      :class-sym 'ede-maven2-project
-	      :new-p nil)
-	     t)
+             (ede-project-autoload "maven2"
+              :name "MAVEN2"
+              :file 'ede-proj-maven2
+              :proj-file "pom.xml"
+              :proj-root 'ede-maven2-project-root
+              :load-type 'ede-maven2-load
+              :class-sym 'ede-maven2-project
+              :new-p nil)
+             t)
 
 (provide 'ede-proj-maven2)
 
