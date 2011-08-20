cb264f741a93ea6118cf342a919a12cee3aab8c9
Eric Ludlam <zappo@gnu.org>
2011-08-20 13:04:26 -0400

    Add header comments and provide.

--- tests/cedet/cogre/convert-utest.el
+++ tests/cedet/cogre/convert-utest.el
@@ -1,6 +1,32 @@
+;;; convert.el --- 
+;;
+;; Copyright (C) 2011 Eric M. Ludlam
+;;
+;; Author: Eric M. Ludlam <eric@siege-engine.com>
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
+;;; Commentary:
+;;
+;; 
+
+;;; Code:
+
 ;;; TESTS
 ;;
-(eval-when-compile (require 'cogre/periodic))
+(require 'cogre/periodic)
+(require 'cogre/convert)
 
 ;;;###autoload
 (defun cogre-export-utest ()
@@ -65,3 +91,8 @@ table as an example."
       )
 
     (message "Graph Conversion to DOT success.")))
+
+
+(provide 'cedet/cogre/convert-utest)
+
+;;; convert.el ends here
