95904291c60232b7d2ea50df5f1cc7c2deddbb68
David Engster <dengste@eml.cc>
2011-11-01 23:11:12 +0200

    [Integration tests] First changes to make them work.
    * srecode/filters.el: Remove requires for `srecode/table' and
      `srecode/insert'. Instead, declare variables and functions
      explicitly.
    
    * srecode/insert.el: Add require for `srecode/filters'.
    
    * tests/cedet-integ-test.el: Fix require `ede/make'. Add
      `srecode/find', remove cit/gnustep.
      (cedet-integ-test-GNUStep): Removed for now.
      (cit-srecode-fill-with-stuff): Activate `semantic-mode'.
    
    * tests/cit-gnustep.el: Fix require.
    
    * tests/cit-load.el: Remove Emacs22 compat.
    
    * tests/cit-test.sh: Fix test call.

--- tests/cit-test.sh
+++ tests/cit-test.sh
@@ -9,7 +9,8 @@ fi
 
 rm -r /tmp/CEDET_INTEG*
 
-if $EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}; then
+cd ..
+if $EMACS -q -l cedet-load.el -l tests/cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}; then
     # Reverse the meaning of a 0 exit status, as the user had to quit Emacs
     # but on success, the program kills emacs with 1 (to be different.)
     exit 1;
