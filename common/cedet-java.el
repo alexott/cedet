c946da4cc87835be4a6156a134d9f14883c68e05
Eric Ludlam <zappo@gnu.org>
2012-02-05 13:35:15 -0500

    (cedet-java-version-check): Split out get-version. (cedet-java-get-version): New, from above. (cedet-jar-table-of-contents): Fix save-excurions warning. (cedet-javap-dump-class): Fix call to cedet-javap-get-class. (cedet-java-jdk-root): New Option (cedet-java-find-jdk): New. (cedet-java-describe): New command.

--- common/cedet-java.el
+++ common/cedet-java.el
@@ -1,6 +1,6 @@
 ;;; cedet-java.el --- Support functions on top of Java's JDK.
 ;;
-;; Copyright (C) 2011 Eric M. Ludlam
+;; Copyright (C) 2011, 2012 Eric M. Ludlam
 ;;
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
 ;;
@@ -74,6 +74,19 @@ If optional programatic argument NOERROR is non-nil, then
 instead of throwing an error if Global isn't available, then
 return nil."
   (interactive)
+  (let ((rev (cedet-java-get-version)))
+    (if (inversion-check-version rev nil cedet-java-min-version)
+	(if noerror
+	    nil
+	  (error "Version of Java is %s.  Need at least %s"
+		 rev cedet-java-min-version))
+      ;; Else, return TRUE, as in good enough.
+      (when (cedet-called-interactively-p 'interactive)
+	(message "Java %s  - Good enough." rev))
+      t)))
+
+(defun cedet-java-get-version ()
+  "Return the version string from executing the java command."
   (let ((b (condition-case nil
 	       (cedet-java-call (list "-version"))
 	     (error nil)))
@@ -86,16 +99,8 @@ return nil."
       (with-current-buffer b
 	(goto-char (point-min))
 	(re-search-forward "java version \"\\([0-9._]+\\)\"" nil t)
-	(setq rev (match-string 1))
-	(if (inversion-check-version rev nil cedet-java-min-version)
-	    (if noerror
-		nil
-	      (error "Version of Java is %s.  Need at least %s"
-		     rev cedet-java-min-version))
-	  ;; Else, return TRUE, as in good enough.
-	  (when (cedet-called-interactively-p 'interactive)
-	    (message "Java %s  - Good enough." rev))
-	  t)))))
+	(setq rev (match-string 1))))
+    rev))
 
 ;;; Java "jar" command Support
 ;;
@@ -105,7 +110,8 @@ Return the contents as a list of paths to files.
 Exclude empty directories."
   (let* ((b (cedet-jar-call (list "-tf" jarfile)))
 	 (strs (split-string
-		(save-excursion (set-buffer b) (buffer-string))
+		(save-current-buffer
+		  (set-buffer b) (buffer-string))
 		"\n" t))
 	 (ans nil))
     (dolist (C strs)
@@ -159,9 +165,135 @@ Exclude empty directories."
   "Dump out a Java signatures for CLASS.
 Display in a javap output buffer."
   (interactive "sClass: ")
-  (switch-to-buffer (cedet-javap-get-class class)))
+  (switch-to-buffer (cedet-javap-get-class nil class)))
 
 
+;;; JAVA Platform Specific Hacks/Detection
+;;
+;; Thanks Joel Borggren-Franck for the data needed for the first pass
+;;
+;; He said:
+;; > But here is a list to get you started looking:
+;; > - Oracle supplied bundle: JAVA_HOME/jre/lib/rt.jar
+;; > - Mac OS X JDK 6:
+;; > /Library/Java/JavaVirtualMachines/1.6.0_29-b11-402.jdk/Contents/Classes/classes.jar
+;; > - Max OS X Oracle JDK 7 Preview: /Library/Java/JavaVirtualMachines/JDK
+;; > 1.7.0 Developer Preview.jdk/Contents/Home/jre/lib/rt.jar
+;; > - On Windows JAVA_HOME is usually a variation of C:\Program Files\Java ...
+(defcustom cedet-java-jdk-root nil
+  "Specify the root location of your JDK.
+If nil, the root will be derived as needed.  Specify this variable if
+the JDK cannot be found.
+Examples:
+ MACOS: /Library/Java/JavaVirtualMachines/1.6.0_29-b11-402.jdk/
+ Windows: C:\Program Files\Java\
+ Linux: /usr/local/lib/jvm/java-6-openjdk/"
+  :group 'java
+  :type 'string)
+
+(defun cedet-java-find-jdk ()
+  "Return the location of the JDK core .jar file."
+  (let ((javahome (getenv "JAVA_HOME"))
+	)
+    (cond
+
+     ;; User specified.
+     (cedet-java-jdk-root
+      (expand-file-name "jre/lib/rt.jar" cedet-java-jdk-root))
+
+     ;; WITH ORACLE Environment Variable
+     (javahome
+      (expand-file-name "jre/lib/rt.jar" javahome))
+
+     ;; MACOS
+     ((eq system-type 'darwin)
+      (let* ((libtmp "/Library/Java/JavaVirtualMachines/")
+	     (libtmpb  (expand-file-name "JDK" libtmp))
+	     (lib (if (file-exists-p libtmpb) libtmpb libtmp))
+	     (file "Contents/Classes/classes.jar")
+	     (dirf (directory-files lib nil ".jdk" nil)))
+	;; The first match in dirf is the oldest (alpha sort = oldest # first)
+	(expand-file-name file (expand-file-name (car (last dirf)) lib))))
+     
+     ;; WINDOWS
+     ((eq system-type 'windows-nt)
+      (let* ((lib "c:/program files/java/jre/lib/")
+	     (file "rt.jar"))
+	;; The first match in dirf is the oldest (alpha sort = oldest # first)
+	(expand-file-name file lib)))
+      
+     ;; Misc Unix (tested on Linux)
+     ;; /usr/lib/jvm/java-6-sun/jre/lib/rt.jar
+     ;; /usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar
+     (t
+      (let ((basedirs '("/usr/lib/jvm" "/usr/local/lib/jvm"))
+	    (dflt "default-java")
+	    (jdks '("sun" "openjdk" "gcj"))
+	    dirs
+	    (file "jre/lib/rt.jar")
+	    base jdk)
+	;; Find base dir
+	(while (and (not base) basedirs)
+	  (when (file-exists-p (car basedirs))
+	    (setq base (car basedirs))))
+	(catch 'nojdk
+	  ;; Find the right JDK to use.
+	  (if (file-exists-p (expand-file-name dflt base))
+	      (expand-file-name file (expand-file-name dflt base))
+	    (throw 'nojdk nil))
+	  ;; No magic default via OS.  Use directory-files.
+	  (while (and jdks (not jdk))
+	    ;; Should we call cedet-java-get-version, and use that in
+	    ;; our query???	    
+	    (setq dirs (directory-files base nil (car jdks) nil))
+	    (when (and dirs (file-exists-p (expand-file-name (car (last dirs)) base)))
+	      (setq jdk (car (last dirs)))))
+	  (unless jdk (throw 'nojdk nil))
+	  ;; Full expand
+	  (expand-file-name file (expand-file-name jdk base)))))
+     )))
+
+(defun cedet-java-describe ()
+  "Describe the discernable java environment."
+  (interactive)
+
+  (with-output-to-temp-buffer "*CEDET Java Environment*"
+
+    (princ "CEDET Java Operational Environment:\n\n")
+    (princ "Current Java Version: ")
+    (princ (cedet-java-get-version))
+    (princ "\n\nMinimum Desired version: ")
+    (princ cedet-java-min-version)
+    (princ "\nJava Command: ")
+    (princ cedet-java-command)
+    (princ "\nJar Command: ")
+    (princ cedet-jar-command)
+    (princ "\nJavap Command: ")
+    (princ cedet-javap-command)
+
+    ;; CLASSPATH
+    (princ "\n\nUser Specified Global Classpath Extension: \n")
+    (dolist (P cedet-java-classpath-extension)
+      (princ "  >  ")
+      (princ P)
+      (princ "\n"))
+
+    ;; EDE
+    (when (and (or (featurep 'ede) (featurep 'cedet/ede))
+	       (ede-current-project))
+      (let ((jcp (ede-java-classpath (ede-current-project))))
+	(princ "\n\nProject Specified Classpath Extension (EDE): \n")
+	(dolist (P jcp)
+	  (princ "  >  ")
+	  (princ P)
+	  (princ "\n"))))
+
+    ;; JDK home
+    (princ "\n\nJava JDK core Jar file: \n  ")
+    (let ((jdkhome (cedet-java-find-jdk)))
+      (princ (or jdkhome "No JDK Found.
+   Set cedet-java-jdk-root to a valid jdk root.\n")))
+    ))
 
 (provide 'cedet-java)
 
