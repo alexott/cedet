78a21bbba8ec9c8c35f730ce56434c6011f4198b
Eric Ludlam <zappo@gnu.org>
2012-03-12 18:18:37 -0400

    (semantic-symref-find-tags-by-name): Fix doc.

--- semantic/symref/semantic-symref.el
+++ semantic/symref/semantic-symref.el
@@ -180,7 +180,7 @@ to perform the search.  This was added for use by a test harness."
 
 ;;;###autoload
 (defun semantic-symref-find-tags-by-name (name &optional scope)
-  "Find a list of references to NAME in the current project.
+  "Find a list of tags by NAME in the current project.
 Optional SCOPE specifies which file set to search.  Defaults to 'project.
 Refers to `semantic-symref-tool', to determine the reference tool to use
 for the current buffer.
